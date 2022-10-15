7aabc41dee63f379ab81b28bb25fb3b1a68e9e22
Courtney Webster <courtney@atlmd.net>
2022-10-14 21:34:54 -0400

    set up testing

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import App from './App';
+
+describe('App', () => {
+  it('Renders hello world!', () => {
+    // ARRANGE
+    //ACT
+    //EXPECT
+  });
+});
