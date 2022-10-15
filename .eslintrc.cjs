3724e7fa3c6bee67149ddf54ff325729b0d5c04e
Courtney Webster <courtney@atlmd.net>
2022-10-14 20:00:39 -0400

    added eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb',
+        'airbnb/hooks',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'react/react-in-jsx-scope': 0,
+        'import/no-extraneous-dependencies': 0,
+    },
+};
